87676d0fe5fea6ee00f50f693dc21c0f52767244
samytichadou <samytichadou@gmail.com>
2023-06-20 16:26:11 +0200

    UI

--- gui.py
+++ gui.py
@@ -29,16 +29,6 @@ class RNDRP_PT_render_presets(bpy.types.Panel):
 
         layout = self.layout
 
-        row = layout.row(align=True)
-        row.operator("rndrp.reload_presets")
-        row.operator("rndrp.create_preset")
-
-        # If active preset
-        if props.active_preset_index in range(0,len(props.presets)):
-            active_preset = props.presets[props.active_preset_index]
-            op = row.operator("rndrp.apply_preset")
-            op.preset_name = active_preset.name
-
         row = layout.row()
         row.template_list(
             "RNDRP_UL_available_presets",
@@ -47,8 +37,27 @@ class RNDRP_PT_render_presets(bpy.types.Panel):
             "presets",
             props,
             "active_preset_index",
-            rows = 3,
+            rows = 4,
             )
+
+        col = row.column(align=True)
+        col.operator("rndrp.reload_presets", icon='FILE_REFRESH', text="")
+        col.separator()
+        col.operator("rndrp.create_preset", icon='ADD', text="")
+
+        sub = col.column(align=True)
+        # TODO replace with remove
+        op1 = sub.operator("rndrp.create_preset", icon='REMOVE', text="")
+        sub.separator()
+        op2 = sub.operator("rndrp.apply_preset", icon="CHECKMARK", text="")
+
+        # Deal with active preset or not
+        if props.active_preset_index in range(0,len(props.presets)):
+            active_preset = props.presets[props.active_preset_index]
+            # op1.preset_name = active_preset.name
+            op2.preset_name = active_preset.name
+        else:
+            sub.enabled = False
 
 
 ### REGISTER ---
