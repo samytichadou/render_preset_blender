b2e55ebd806ffa06215baa930a3dc25a747387be
samytichadou <samytichadou@gmail.com>
2023-06-20 17:18:49 +0200

    add properties to store

--- render_properties.py
+++ render_properties.py
@@ -1,7 +1,9 @@
 render_properties = {}
 
-# render
+# scene.render
 render_properties["scene.render"] = [
+    "engine",
+
     "resolution_x",
     "resolution_y",
     "resolution_percentage",
@@ -20,16 +22,65 @@ render_properties["scene.render"] = [
     "use_overwrite",
     "use_placeholder",
 
-    #TODO Metadatas
+    "use_simplify",
+
+    "use_stamp",
+    "use_stamp_date",
+    "use_stamp_time",
+    "use_stamp_render_time",
+    "use_stamp_frame",
+    "use_stamp_frame_range",
+    "use_stamp_memory",
+    "use_stamp_hostname",
+    "use_stamp_camera",
+    "use_stamp_lens",
+    "use_stamp_scene",
+    "use_stamp_marker",
+    "use_stamp_filename",
+    "use_stamp_sequencer_strip",
+
+    "film_transparent",
     ]
 
-# render.image_settings
+# scene.render.image_settings
 render_properties["scene.render.image_settings"] = [
     "file_format",
     "color_mode",
     "compression",
     "color_depth",
+    "quality",
     #TODO Formats details (video...)
+    ]
+
+# scene.cycles
+render_properties["scene.cycles"] = [
+    "device",
+    "feature_set",
+
+    "dicing_rate",
+
+    "use_denoising",
+    "denoiser",
+    "adaptive_threshold",
+
+    "use_auto_tile",
+    "tile_size",
+
+    "caustics_reflective",
+    "caustics_refractive",
+
+    "samples"
+    ]
+
+# scene.view_settings
+render_properties["scene.view_settings"] = [
+    "view_transform",
+    ]
+
+# scene.cgru
+render_properties["scene.cgru"] = [
+    "fpertask",
+    "priority",
     ]
 
 # Disabled by default
